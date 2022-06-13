899a6e9d69ae93ff144864bf669b89b8bbc3e6b5
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2022-06-12 20:18:17 -0400

    chore(lint): Fix lint

--- index.ts
+++ index.ts
@@ -9,7 +9,7 @@ const config: BaseConfig = {
     "no-negated-condition": "off",
     "no-restricted-syntax": "off",
     "no-warning-comments": "off",
-    "no-undef": "off"
+    "no-undef": "off",
   },
 };
 
